492736426f95c93bc26c264e17f921ff2e1252de
duanpeizhou <duanpeizhou@163.com>
2018-09-20 23:53:46 +0800

    add error page

--- contraceptive-management-system-web/src/main/java/cn/zectec/contraceptive/management/system/web/controller/LoginController.java
+++ contraceptive-management-system-web/src/main/java/cn/zectec/contraceptive/management/system/web/controller/LoginController.java
@@ -86,5 +86,10 @@ public class LoginController {
 	public String noPermissionUI(){
 		return "noPermission";
 	}
+
+	@RequestMapping("/errorPage")
+	public String errorPage() {
+		return "errorPage";
+	}
 	
 }
